3d40229bf4e09c688387c1e30b707e97b780bb7a
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-01-31 11:01:24 +0530

    test changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -65,7 +65,7 @@ export const Banner = () => {
                 >
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>
-                    {`Hi! I'm Vishal`}{" "}
+                    {`Hi! I'm Viishal`}{" "}
                     <span
                       className="txt-rotate"
                       data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'
