1c694492c3a8413c8dfbd0dee92e6b1cb8536369
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-24 10:54:40 +0530

    made responsive contact

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = ["Web Developer", "Graphics Designer", "Video Editor"];
+  const toRotate = ["Web Developer", "Graphic Designer", "Video Editor"];
   const period = 2000;
 
   useEffect(() => {
