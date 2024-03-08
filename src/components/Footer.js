ad3bbf1026457331e72d136a555578d1ce7bebcd
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-03-08 13:14:14 +0530

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,8 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <Row className="align-items-center">
-          <MailchimpForm />
+        <Row className="align-items-center py-4">
+          {/* <MailchimpForm /> */}
           <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
